7ef92ff274512831fcab50bf6f4011d020b9edd0
Cameron Little <cameron@camlittle.com>
2015-09-25 12:39:15 -0700

    Initial Commit

---
+++ LiveGifs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiveGifs
+//
+//  Created by Cameron Little on 9/25/15.
+//  Copyright © 2015 Cameron Little. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
